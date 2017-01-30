6e3e90053dfa81a7caa5172f8ec5980b71c99bf0
Brian Doane <brianmdoane@gmail.com>
2017-01-29 21:57:24 -0600

    installed inert which is registered by hapi server, with error handling to handle static html files

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const Hapi = require('hapi')
 const server = new Hapi.Server()
 server.connection({ port: 3000, host: 'localhost' })
 
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html')
+        }
+    })
+})
+
 server.route({
   method: 'GET',
   path: '/',
