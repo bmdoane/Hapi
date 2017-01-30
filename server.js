e216d89053ffc58340fba44ea0b844609ea12835
Brian Doane <brianmdoane@gmail.com>
2017-01-29 21:23:11 -0600

    comments and some syntax clean up

--- server.js
+++ server.js
@@ -6,25 +6,26 @@ const server = new Hapi.Server()
 server.connection({ port: 3000, host: 'localhost' })
 
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!')
-    }
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!')
+  }
 })
 
 server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply(`Hello ${encodeURIComponent(request.params.name)}!`)
-    }
+  method: 'GET',
+  path: '/{name}',
+  handler: function (request, reply) {
+  	// To prevent content injection attacks
+    reply(`Hello ${encodeURIComponent(request.params.name)}!`)
+  }
 })
 
 server.start((err) => {
 
-    if (err) {
-        throw err
-    }
-    console.log(`Server running at: ${server.info.uri}`)
+  if (err) {
+    throw err
+  }
+  console.log(`Server running at: ${server.info.uri}`)
 })
